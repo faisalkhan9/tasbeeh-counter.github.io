3d9c8140f347809d00c7bc6e0d322eb7db45bcd4
faisalkhan9 <86875342+faisalkhan9@users.noreply.github.com>
2023-09-20 11:44:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 
 let a = /* document.getElementById('count').innerHTML = */ localStorage.getItem('name');
 if (a == null) {
-  count = 0;
+ let count = 0;
         document.getElementById('count').innerHTML = count;
         localStorage.setItem('name', JSON.stringify(count));
+document.getElementById('increaseBtn').onclick = function() {
+    /*  let click = document.getElementById('click').play(); */
+    count += 1;
+    document.getElementById('count').innerHTML = count;
+    localStorage.setItem('name', JSON.stringify(count));
+  }
+  document.getElementById('resetBtn').onclick = function() {
+
+    let reset = confirm("Click ok to reset");
+    if (reset) {
+      count = 0;
+      document.getElementById('count').innerHTML = count;
+      localStorage.setItem('name', JSON.stringify(count))
+    }
+    else {
+      localStorage.getItem('name');
+    }
+  
 }
+  
+  
+}
+
 else {
-let a = localStorage.getItem('name');
   let count = parseInt(a);
   document.getElementById('increaseBtn').onclick = function() {
     /*  let click = document.getElementById('click').play(); */
