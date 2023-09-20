ba4ae3aedaf7b72f9a76f3435a8b49eb475df8f4
faisalkhan9 <86875342+faisalkhan9@users.noreply.github.com>
2023-09-20 11:37:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 
+let a = /* document.getElementById('count').innerHTML = */ localStorage.getItem('name');
+if (a == null) {
+  count = 0;
+        document.getElementById('count').innerHTML = count;
+        localStorage.setItem('name', JSON.stringify(count));
+}
+else {
+let a =  document.getElementById('count').innerHTML =  localStorage.getItem('name');
+  let count = parseInt(a);
+  document.getElementById('increaseBtn').onclick = function() {
+    /*  let click = document.getElementById('click').play(); */
+    count += 1;
+    document.getElementById('count').innerHTML = count;
+    localStorage.setItem('name', JSON.stringify(count));
+  }
+  document.getElementById('resetBtn').onclick = function() {
 
-
-  
-   
-    let a = document.getElementById('count').innerHTML = localStorage.getItem('name'); 
-    if (a == null) {
+    let reset = confirm("Click ok to reset");
+    if (reset) {
       count = 0;
+      document.getElementById('count').innerHTML = count;
+      localStorage.setItem('name', JSON.stringify(count))
     }
     else {
-    let count = parseInt(a); 
-    document.getElementById('increaseBtn').onclick = function() {
-      /*  let click = document.getElementById('click').play(); */
-      count += 1;
-      document.getElementById('count').innerHTML = count;
-      localStorage.setItem('name', JSON.stringify(count));
+      localStorage.getItem('name');
     }
-    document.getElementById('resetBtn').onclick = function() {
-
-      let reset = confirm("Click ok to reset");
-      if (reset) {
-        count = 0;
-        document.getElementById('count').innerHTML = count;
-        localStorage.setItem('name', JSON.stringify(count))
-      }
-      else {
-        localStorage.getItem('name');
-      }
-    }
-    }
+  
+}}
