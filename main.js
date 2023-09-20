a4a5087f2d29bd3927b0e9f70a150729c0e0f760
faisalkhan9 <86875342+faisalkhan9@users.noreply.github.com>
2023-09-20 12:08:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,28 @@
+setTimeout (function() {
+  alert("reset to start (small button)")
+},3000);
 
-let a = /* document.getElementById('count').innerHTML = */ localStorage.getItem('name');
-if (a == null) {
- let count = 0;
-        document.getElementById('count').innerHTML = count;
-        localStorage.setItem('name', JSON.stringify(count));
+
+
+localStorage.getItem('name');
+let a = document.getElementById('count').innerHTML = localStorage.getItem('name');
+let count = parseInt(a);
 document.getElementById('increaseBtn').onclick = function() {
-    /*  let click = document.getElementById('click').play(); */
-    count += 1;
-    document.getElementById('count').innerHTML = count;
-    localStorage.setItem('name', JSON.stringify(count));
-  }
-  document.getElementById('resetBtn').onclick = function() {
-
-    let reset = confirm("Click ok to reset");
-    if (reset) {
-      count = 0;
-      document.getElementById('count').innerHTML = count;
-      localStorage.setItem('name', JSON.stringify(count))
-    }
-    else {
-      localStorage.getItem('name');
-    }
-  
+  /*  let click = document.getElementById('click').play(); */
+  count += 1;
+  document.getElementById('count').innerHTML = count;
+  localStorage.setItem('name', JSON.stringify(count));
 }
-  
-  
+document.getElementById('resetBtn').onclick = function() {
+
+  let reset = confirm("Click ok to reset");
+  if (reset) {
+    count = 0;
+    document.getElementById('count').innerHTML = count;
+    localStorage.setItem('name', JSON.stringify(count))
+  }
+  else {
+    localStorage.getItem('name');
+  }
 }
 
-else {
-  let count = parseInt(a);
-  document.getElementById('increaseBtn').onclick = function() {
-    /*  let click = document.getElementById('click').play(); */
-    count += 1;
-    document.getElementById('count').innerHTML = count;
-    localStorage.setItem('name', JSON.stringify(count));
-  }
-  document.getElementById('resetBtn').onclick = function() {
-
-    let reset = confirm("Click ok to reset");
-    if (reset) {
-      count = 0;
-      document.getElementById('count').innerHTML = count;
-      localStorage.setItem('name', JSON.stringify(count))
-    }
-    else {
-      localStorage.getItem('name');
-    }
-  
-}}
